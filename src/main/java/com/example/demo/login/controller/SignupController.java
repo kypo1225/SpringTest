26c6a669609453243bb2c48ebf616f9faa13d3ba
usamin_torein <usamin_torein@192.168.50.157>
2019-09-03 18:50:26 +0900

    バリテーションテスト

---
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -0,0 +1,53 @@
+package com.example.demo.login.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.login.domain.SignupForm;
+
+@Controller
+public class SignupController {
+	private Map<String, String> radioMarriage;
+	private Map<String, String>initRadioMarriage(){
+		//ラジオボタンの初期化
+		Map<String, String> radio = new LinkedHashMap<>();
+		//既婚、未婚をMapに格納
+		radio.put("既婚", "true");
+		radio.put("未婚", "false");
+		return radio;
+		
+	}
+	
+	@RequestMapping(value = "/signup")
+	public String getSignUp(@ModelAttribute SignupForm form, Model model) {
+		//ラジオボタンの初期化のメソッドを呼ぶ
+		radioMarriage = initRadioMarriage();
+		//ラジオボタン用のMapをmodelに登録
+		model.addAttribute("radioMarriage", radioMarriage);
+		
+		return "login/signup";
+	}
+	
+	@PostMapping("/signup")
+	//フォームクラスをmodelに登録
+	//@Validatedでバリテーション実施。BindingResultクラスに入っている
+	public String postSignUp(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
+		//データバインドの失敗の場合、登録画面に戻る
+		if(bindingResult.hasErrors()) {
+			//Getリクエストメソッドを呼び出す。
+			return getSignUp(form, model);
+		}
+		System.out.println(form);
+		
+		return "redirect:/login";
+	}
+
+}
