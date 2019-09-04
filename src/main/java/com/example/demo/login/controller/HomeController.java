f4320096a962f2a808a9205ef12a1adb640a13de
usamin_torein <usamin_torein@192.168.50.157>
2019-09-04 18:49:42 +0900

    バリテーション、AOP、プリプロセッシング

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -11,8 +11,17 @@ public class HomeController {
 	
 	@RequestMapping(value = "/home")
 	public String getHome(Model model) {
+		
 		model.addAttribute("contents", "login/home :: home_contents");
+		
 		return "login/homeLayout";
+	}
+	
+	@RequestMapping("/logout")
+	public String getLogout() {
+		
+		return "redirect:/login";
+		
 	}
 
 }
